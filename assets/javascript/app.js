e0a5c9f6a80da247b5b1b8949ac5de4c123acc06
Carolyn Grady <owenv8@gmail.com>
2020-01-24 18:09:10 -0500

    added create buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,52 @@
 $(document).ready(function () {
 
-var quesries = [];
+    var queries = [];
 
-$('#submit-btn').on('click', function (event) {
-    event.preventDefault();
-    console.log(this)
-    // get users search query from the text box
-    var query = $('#userQuery').val().trim()
+    $('#submit-btn').on('click', function (event) {
+        event.preventDefault();
+        console.log(this)
 
-    // add the search query to the queries array
-    //queries.push(query);
-    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=l3BQXaVrqxak9wat1iZYciOAzYiTQlI1&q=' + query + '&limit=10&offset=0&rating=PG&lang=en'
-    createButtons();
-})
+        // get users search query from the text box
+        var query = $('#userQuery').val().trim()
 
-function createButtons(){
+        // add the search query to the queries array
+        queries.push(query);
+        console.log(query)
 
-    for (var i = 0; i < quesries.length; i++){
-        var btn = $('<button>');
+        createButtons();
+    })
 
-        btn.addClass('gif').attr('value', quesries[i]).text(quesries[i]);
+    function createButtons() {
 
-        $('.buttons').append(btn);
+        for (var i = 0; i < queries.length; i++) {
+            var btn = $('<button>');
+
+            btn.addClass('btn btn-primary btn gif').attr('value', queries[i]).text(queries[i]);
+        }
+
+        $('#userQuery').empty();
+        $('.buttons').prepend(btn);
     }
-}
+
+    function showGIF() {
+        var userQuery = $(this).attr('value')
+        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=l3BQXaVrqxak9wat1iZYciOAzYiTQlI1&q=' + userQuery + '&limit=10&offset=0&rating=PG&lang=en'
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+            for ( var i = 0; response.data.images.length; i++){
+                var image = $('<img>').attr('src', response.data.images.url);
+
+                $('.main').append(image);
+            }
+        })
+    }
+
+    $(document).on('click', '.gif', showGIF);
+
+    createButtons();
+
 
 })
