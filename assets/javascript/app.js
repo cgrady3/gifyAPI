bb49b85ef7da23558d4d15fc4115a59274afa5b7
Carolyn Grady <owenv8@gmail.com>
2020-01-27 16:19:15 -0500

    fixed query button creation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,82 @@
+var queries = ['funny cat', 'flowers', 'rainbow'];
+
+function createButtons() {
+
+    for (let i = 0; i < queries.length; i++) {
+
+        var btn = $('<a>');
+
+        btn.addClass('btn btn-primary btn gif').text(`${queries[i]}`);
+        btn.attr({
+            'value': `${queries[i]}`,
+            'type': 'submit',
+            'role': 'button',
+        })
+        $('.buttons').prepend(btn);
+    }
+}
+
+function showGIF() {
+
+    var userQuery = $(this).attr('value')
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=l3BQXaVrqxak9wat1iZYciOAzYiTQlI1&q=' + userQuery + '&limit=10&offset=0&rating=PG&lang=en'
+
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response.data);
+
+        for (let i = 0; response.data.images.length; i++) {
+            var image = $('<img>').attr('src', response.data.images[i].url);
+
+            $('.main').prepend(images);
+        }
+    })
+}
+
 $(document).ready(function () {
 
-    var queries = [];
+
+
+    console.log(queries)
 
     $('#submit-btn').on('click', function (event) {
+        // prevent a button click from opening up to a new page
         event.preventDefault();
         console.log(this)
 
-        // get users search query from the text box
-        var query = $('#userQuery').val().trim()
+        // check that the user has entered a search term
+        if ($('#userQuery').val() === '') {
+            alert('Please enter a valid search term')
+        }
+        else {
+            // get users search query from the text box
+            var query = $('#userQuery').val().trim()
 
-        // add the search query to the queries array
-        queries.push(query);
-        console.log(query)
+            // add the search query to the queries array
+            queries.push(query);
 
-        createButtons();
+            // add new queries as buttons
+            $('#userQuery').text('');
+            var btn = $('<a>');
+
+            btn.addClass('btn btn-primary btn gif').text(`${queries[queries.length-1]}`);
+            btn.attr({
+                'value': `${queries[queries.length-1]}`,
+                'type': 'submit',
+                'role': 'button',
+            })
+            $('.buttons').prepend(btn);
+        }
     })
 
-    function createButtons() {
 
-        for (var i = 0; i < queries.length; i++) {
-            var btn = $('<button>');
 
-            btn.addClass('btn btn-primary btn gif').attr('value', queries[i]).text(queries[i]);
-        }
 
-        $('#userQuery').empty();
-        $('.buttons').prepend(btn);
-    }
-
-    function showGIF() {
-        var userQuery = $(this).attr('value')
-        var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=l3BQXaVrqxak9wat1iZYciOAzYiTQlI1&q=' + userQuery + '&limit=10&offset=0&rating=PG&lang=en'
-
-        $.ajax({
-            url: queryURL,
-            method: 'GET'
-        }).then(function (response) {
-            for ( var i = 0; response.data.images.length; i++){
-                var image = $('<img>').attr('src', response.data.images.url);
-
-                $('.main').append(image);
-            }
-        })
-    }
 
     $(document).on('click', '.gif', showGIF);
 
     createButtons();
-
-
+    //showGIF();
 })
